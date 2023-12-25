c2b45e0332f618f73d358a1a3b364181a75c442a
farhanakim <128131592+farhanakim@users.noreply.github.com>
2023-12-25 18:59:44 +0800

    Did the javascript part of web app

---
+++ script.js
@@ -0,0 +1,34 @@
+/* To show city and API integration*/
+function showCity(event) {
+    event.preventDefault(); //This line prevents the default behavior of the form submission, which would normally be to reload the page. It allows the JavaScript code to handle the submission without a page refresh.
+    let searchCity = document.querySelector("#search-input");
+    let cityElement = document.querySelector("#current-city");
+    cityElement.innerHTML = searchCity.value.toLowerCase().replace(/\b\w/g, l => l.toUpperCase());
+
+    let city = searchCity.value;
+    let apiKey = "410fa504959827feb595o996c3d3et87";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(displayWeather);
+}
+
+function displayWeather(response) {
+    let temperatureElement = document.querySelector(".current-temperature-value")
+    let temperature = Math.round(response.data.temperature.current);
+    temperatureElement.innerHTML = temperature;
+}
+
+let searchForm = document.querySelector("#search-form"); 
+searchForm.addEventListener("submit", showCity); //This line attaches an event listener to the searchForm. It listens for the "submit" event (when the form is submitted) and calls the showCity function when that event occurs.
+
+/* To show date */
+let now = new Date();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+let currentDay = days[now.getDay()];
+let currentDate = now.getDate();
+let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let currentMonth = month[now.getMonth()];
+let currentHour = now.getHours();
+let currentMinute = now.getMinutes();
+let currentTime = document.querySelector("#current-date");
+currentTime.innerHTML = (`${currentDay}, ${currentDate} ${currentMonth}, ${currentHour}:${currentMinute}`);
